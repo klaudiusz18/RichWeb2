538b345f3ce8d0b36e3dd0ced39631715fe57bd8
klaudiusz <klaudiusz443@gmail.com>
2022-11-02 17:42:51 +0000

    added a wordfrequency function

--- Lab2/Problem 2/index.js
+++ Lab2/Problem 2/index.js
@@ -3,3 +3,17 @@ fetch('http://jsonplaceholder.typicode.com/posts?')
 .then(json => json.filter(y => y.title.split("").length > 6))
 .then(result => result.map(y => y.title))
 .then((data) => console.log(data));
+
+function wordfreq(string) {
+    var words = string.replace(/[.]/g, '').split(/\s/);
+    var map = {};
+    words.forEach(function(words) {
+        if(!map[word]) {
+            map[word] = 0;
+        }
+        map[word] +1;
+
+    });
+
+    return map;
+}
