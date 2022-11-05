b69585d6d6442cc71faadff880943d5a6095a91b
klaudiusz <klaudiusz443@gmail.com>
2022-11-05 23:40:49 +0000

    added contact summary - without any validation

--- Lab2/Problem 1/index.js
+++ Lab2/Problem 1/index.js
@@ -1,16 +1,30 @@
-var form = document.getElementById("form-id");
-form.addEventListener("click", diplayDetails);
 
+var button = document.getElementById("button");
+button.addEventListener("click", displayDetails);
 var row = 1;
 
-function diplayDetails() {
+function displayDetails() {
     var name = document.getElementById("name").value;
     var number = document.getElementById("number").value;
     var email = document.getElementById("email").value;
 
-    if(!name || !number || !email) {
-        alert("Please fill in all boxes");
-        return;
-    }
+
+
+
+
+    var display = document.getElementById("results");
+
+    var newRow = display.insertRow(row);
+
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+    var cell3 = newRow.insertCell(2);
+
+    cell1.innerHTML = name;
+    cell2.innerHTML = number;
+    cell3.innerHTML = email;
+
+
+    row++;
 
 }
