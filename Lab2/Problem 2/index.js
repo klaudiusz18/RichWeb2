c9bff42ca5b363858875242e7d27ce10769bddb7
klaudiusz <klaudiusz443@gmail.com>
2022-11-05 19:51:27 +0000

    added a wordfrequency count

--- Lab2/Problem 2/index.js
+++ Lab2/Problem 2/index.js
@@ -1,19 +1,26 @@
-fetch('http://jsonplaceholder.typicode.com/posts?')
-.then(Response => Response.json())
-.then(json => json.filter(y => y.title.split("").length > 6))
-.then(result => result.map(y => y.title))
-.then((data) => console.log(data));
+fetch('http://jsonplaceholder.typicode.com/posts?')  
+	.then(response => response.json()) 
+	.then(json => json.filter(y => y.title.split("").length > 6))
+	.then(result => result.map(y => y.title))
+	.then((data) => console.log(data)); 
+	
+	
+      function wordfreq(string) {  
+		  var words =string.replace(/[.]/g, '').split(/\s/);
+		  var map = {};
+		  words.forEach(function(word) {
+			  if (!map[word]) {
+				  map[word] = 0;
+			  }
+			  map[word] +=1;
+		  });
+		  
+		  return map;
 
-function wordfreq(string) {
-    var words = string.replace(/[.]/g, '').split(/\s/);
-    var map = {};
-    words.forEach(function(word) {
-        if(!map[word]) {
-            map[word] = 0;
-        }
-        map[word] +1;
+       
+	  }
 
-    });
-
-    return map;
-}
+      fetch('http://jsonplaceholder.typicode.com/posts?')
+          .then(response => response.json())
+          .then(result => result.forEach(json => console.log(wordfreq(json.body))));    
+	
